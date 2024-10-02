c0384dedbc28f4e87ef05f2c81b81f897a57208a
Raúl Blanco <64635695+raulblancoo@users.noreply.github.com>
2024-10-02 19:10:02 +0200

    First commit

---
+++ ComPay/src/main/java/com/tsw/ComPay/ComPayApplication.java
@@ -0,0 +1,13 @@
+package com.tsw.ComPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComPayApplication.class, args);
+	}
+
+}
