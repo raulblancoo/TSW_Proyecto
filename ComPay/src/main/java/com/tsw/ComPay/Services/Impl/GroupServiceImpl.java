2c081b7f1f3a57f5837647e5116d82a9d974f403
Raúl Blanco <64635695+raulblancoo@users.noreply.github.com>
2024-10-14 22:04:44 +0200

    Implementaciones servicios de grupos

--- ComPay/src/main/java/com/tsw/ComPay/Services/Impl/GroupServiceImpl.java
+++ ComPay/src/main/java/com/tsw/ComPay/Services/Impl/GroupServiceImpl.java
@@ -1,11 +1,17 @@
 package com.tsw.ComPay.Services.Impl;
 
 import com.tsw.ComPay.Dto.GroupDto;
+import com.tsw.ComPay.Dto.NewGroupDto;
+import com.tsw.ComPay.Dto.UserDto;
 import com.tsw.ComPay.Models.GroupModel;
+import com.tsw.ComPay.Models.UserModel;
 import com.tsw.ComPay.Repositories.GroupRepository;
 import com.tsw.ComPay.Services.GroupService;
+import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.swing.*;
 
 @Service
 public class GroupServiceImpl implements GroupService {
@@ -13,14 +19,17 @@ public class GroupServiceImpl implements GroupService {
     @Autowired
     private GroupRepository groupRepository;
 
-    @Override
-    public void saveGroup(GroupDto groupDTO) {
+    public void saveGroup(NewGroupDto groupDto) {
         GroupModel group = new GroupModel();
 
-        group.setGroupname(groupDTO.getGroupname());
-        group.setImageURL(groupDTO.getImageURL());
-        group.setAmount(groupDTO.getAmount());
+        group.setGroupName(groupDto.getGroupName());
+        group.setCurrency(groupDto.getCurrency());
 
         groupRepository.save(group);
     }
+
+    public GroupModel findGroupByName(String groupName){
+        return groupRepository.findByGroupName(groupName);
+    }
+
 }
