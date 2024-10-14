00c2b40a052167210c09f9b0d96accede008420b
Raúl Blanco <64635695+raulblancoo@users.noreply.github.com>
2024-10-14 22:01:41 +0200

    Funcionando el crear grupo (Falta verificar que emails esten en bd para añadirlos)

--- ComPay/src/main/java/com/tsw/ComPay/Controllers/GroupController.java
+++ ComPay/src/main/java/com/tsw/ComPay/Controllers/GroupController.java
@@ -3,6 +3,7 @@ package com.tsw.ComPay.Controllers;
 
 import com.tsw.ComPay.Dto.NewGroupDto;
 import com.tsw.ComPay.Enums.CurrencyEnum;
+import com.tsw.ComPay.Services.GroupMembersService;
 import com.tsw.ComPay.Services.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,26 +23,38 @@ public class GroupController {
     @Autowired
     private GroupService groupService;
 
+    @Autowired
+    private GroupMembersService groupMembersService;
+
     @GetMapping("/show")
     public String showGroups(Model model) {
+        //TODO: Mapper e implementar funciones para listar grupo
+        //List<NewGroupDto> groups = groupService.getGroups(); // Assuming you have a method to retrieve the groups
+        //model.addAttribute("groups", groups);
         return "groups/groups"; // Retornamos la vista principal
     }
 
     @GetMapping("/create")
     public String createGroup(Model model) {
-        //List<String> currency = Arrays.asList(CurrencyEnum.values().toString());
         model.addAttribute("currency", new NewGroupDto());
+        List<CurrencyEnum> currencies = Arrays.asList(CurrencyEnum.values());
+        model.addAttribute("currencies", currencies);
+
         return "groups/create-group"; // Retorna la vista principal con los fragmentos
     }
 
     @PostMapping("/create")
     public String createGroupPost(Model model, @ModelAttribute("group") NewGroupDto newGroupDto) {
         model.addAttribute("group", newGroupDto);
-        return "";
+        groupService.saveGroup(newGroupDto);
+        groupMembersService.saveGroupMember(newGroupDto);
+        return "redirect:/groups/show";
     }
 
+    // pasarle el nombre del grupo directamente
     @GetMapping("/view-group")
     public String viewGroup(Model model) {
-        return "index/expensesGroup"; // Retorna la vista principal con los fragmentos
+        return "groups/group-expense-layout"; // Retorna la vista principal con los fragmentos
     }
+
 }
