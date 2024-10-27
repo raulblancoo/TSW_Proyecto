3fbac22ccb5e4d7e6a8b21ecfd4938f6cb7a788f
Raúl Blanco <64635695+raulblancoo@users.noreply.github.com>
2024-10-27 14:01:55 +0100

    No changes at all

--- ComPay/src/main/java/com/tsw/ComPay/Controllers/ExpenseController.java
+++ ComPay/src/main/java/com/tsw/ComPay/Controllers/ExpenseController.java
@@ -5,10 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("expense")
+@RequestMapping("expenses")
 public class ExpenseController {
-    @GetMapping("/new")
-    public String showExpense() {
-        return "expenses/expense-layout"; // Retorna la vista principal con los fragmentos
-    }
+
+
+
+
+
 }
