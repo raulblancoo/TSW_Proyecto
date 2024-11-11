475d1bb971ceb862906f9728298cc7d7599b08fa
Anxo <anxorodri95@gmail.com>
2024-11-11 17:25:11 +0100

    correcciones

--- ComPay/src/main/resources/static/js/Validaciones/expendValidaciones.js
+++ ComPay/src/main/resources/static/js/Validaciones/expendValidaciones.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const payerSelect = document.getElementById("payer_select").value;
     const pagoCantidad = document.getElementById("payment_amount").value;
     const conceptoPago = document.getElementById("payment_concept").value;
-    const checkboxUsers = document.getElementById("checkbox").value;
+    const checkboxUsers = document.getElementById("checkbox");
     const dividedPayment = document.getElementById("divided_payment").value;
     const generalErrorContainer = document.getElementById("divErrores"); // Asumiendo que ya existe en el HTML
     generalErrorContainer.style.display = "none"; // Asegurarse que está oculto inicialmente
@@ -15,11 +15,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     form.addEventListener("submit", function (event){
-       event.preventDefault();
+        event.preventDefault();
         let errors = [];
         generalErrorContainer.style.display = "none"; // Limpiar y ocultar errores previos
         generalErrorContainer.querySelector("ul").innerHTML = ""; // Limpiar lista de errores
-
 
         if(!payerSelect.trim() || !pagoCantidad.trim() || !conceptoPago.trim()){
             errors.push("Ningún campo puede ir vacío");
